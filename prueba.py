90ce9f21a1f4e63374bea810f04fc78e24f30730
delfinarawson <drawson@udesa.edu.ar>
2022-11-08 12:40:58 -0300

    punto 2

--- prueba.py
+++ prueba.py
@@ -1,3 +1,6 @@
+
+# 1 listo
+
 archivo = open('bolsa.csv', "r")
 lineas = archivo.readlines()
 archivo.close()
@@ -15,13 +18,31 @@ for linea in lineas[1:]:
 
 lista_meses = [] 
 lista_fechas=[]
+
+# 2 esta parte esta bien 
+
 for fecha in dict['Date']:
-    fecha_split = fecha.split('-')
+    fecha_split = fecha.split('-') # ['2021', '10', '04],['2021', '10', '03]
     ano_mes = fecha_split[0]+"-"+fecha_split[1]
-    mes = fecha_split[1] 
     if ano_mes not in lista_meses:
         lista_meses.append(ano_mes)
-        lista_fechas.append(fecha)
+        lista_fechas.append(fecha) # logramos tener la primer secuencia de fechas
 
-print(lista_fechas)
+# 2 esta parte no nos sale
+
+dict_satl = {}
+dict_meli = {}
+dict_mhvyf = {}
+dict_rtx = {}
+
+lista_valores=[]
+
+for mes in lista_meses: # mes es '2021-10, 2021-11, 2021,12 ...'
+    dict[mes]= 0
+
+for linea in lineas[1:]:
+    linea_split = linea.strip().split(',') # ['2022-08-24', '5.050000190734863', '917.4400024414062', '35.63999938964844', '93.58000183105469']
+    m = linea_split[0][0:7] # 2022-08    
+    if mes == m:
+        dict_satl[mes] = lista_valores
 
--- tp2.py
+++ tp2.py
@@ -19,17 +19,22 @@ print(read_file("bolsa.csv"))
 
 
 def monthly_average (accion, dict):
+    lista_meses = [] 
+    lista_fechas=[]
+
+    cada_mes = []
+    precios = []
+    promedio_meses = []
+    
     for fecha in dict['Date']:
         fecha_split = fecha.split('-')
-        mes = fecha_split[1] 
-        lista_meses = []
-        for mes in dict['Date']:
-            if mes not in lista_meses:
-                lista_meses.append(fecha_split)
+        ano_mes = fecha_split[0]+"-"+fecha_split[1] 
+        if ano_mes not in lista_meses:
+            lista_meses.append(ano_mes)
+            lista_fechas.append(fecha)
+            #['2021-10-04', '2021-11-01', ...] 
 
-
-
-
-
-
-    return(fecha,precios_promedio )
+    for mes in lista_meses:
+        if mes == fecha_split[1]:
+            cada_mes.append(fecha_split)
+        
