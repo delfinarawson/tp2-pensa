6d8333a9b061203ad27fd4912460fef322d0d9bf
antonellaotegui <aotegui@udesa.edu.ar>
2022-11-09 12:31:11 -0300

    hola

--- tp2.py
+++ tp2.py
@@ -1,4 +1,6 @@
 
+# 1
+
 def read_file (archivo):
     archivo = open(archivo, "r")
     lineas = archivo.readlines()
@@ -15,6 +17,8 @@ def read_file (archivo):
             clave = claves[i]
             dict[clave].append(valor)
     return dict
+
+# 2
 
 def monthly_average(accion, diccionario):
 
@@ -35,7 +39,7 @@ def monthly_average(accion, diccionario):
             lista_meses.append(fecha)
             dict[ano_mes] = []
         dict[ano_mes].append(precio)
-        
+    
     precios_prom = []
     
     for valores in dict.values(): # ['9.930000305175781', '9.920000076293945', '9.90999984741211', '9.914999961853027', '9.930000305175781', '9.90999984741211', '9.930000305175781', '9.9399995803833', '9.930000305175781', '9.930000305175781', '9.925999641418457', '9.930000305175781', '9.930000305175781', '9.920000076293945', '9.920000076293945', '9.930999755859375', '10.039999961853027']
@@ -50,3 +54,54 @@ def monthly_average(accion, diccionario):
     return lista_meses, precios_prom
 
 monthly_average('SATL', read_file('bolsa.csv'))
+print(monthly_average('SATL', read_file('bolsa.csv')))
+
+# 3
+archivo = open("monthly_average_SATL.csv", "w")
+
+archivo.write("fechas promedios"+ "\n")
+lista_meses, precios_prom = monthly_average('SATL', read_file('bolsa.csv'))
+i = 0
+while i < len(lista_meses):
+    line = str(lista_meses[i]) +","+ str(precios_prom[i]) + "\n"
+    i += 1
+    archivo.write(line)
+    
+
+archivo.close()
+
+def max_gain (accion,dict,fecha_venta):
+    # precios = dict[accion]
+    # fechas = dict["Date"]
+    # tuplas = zip(precios, fechas)
+
+
+
+    minimo = 10000000
+    index = 0
+    for i, num in enumerate(dict[accion[1:]]):
+        if num < minimo:
+            minimo = num
+            index += i
+    fecha_compra = dict[accion[index]]
+
+    
+"""     lista = list(dict[accion[1:]])
+    minimo = min(lista)
+    print(minimo) """
+
+max_gain("SATL", read_file("bolsa.csv"), "2021-10-04")
+
+
+    # valores_accion = dict[accion]
+    # fecha = dict["date"]
+    # indice = fecha.index(fecha[venta])
+
+
+
+    """ ganancia = (pv-pc) / pc
+    for i, in enumerate()
+ """
+
+
+
